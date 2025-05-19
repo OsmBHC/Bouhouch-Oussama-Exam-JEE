7e5a748bb12bc063c25efe13bc7ef6b7d8557ed4
OsmBHC <oussamabouhouchpro@gmail.com>
2025-05-19 09:48:46 +0100

    Initialisation du projet Spring Boot avec les dépendances pour le backend de gestion de crédits

---
+++ credit-management-backend/src/main/java/ma/enset/oussama/bouhouch/creditmanagementbackend/CreditManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.oussama.bouhouch.creditmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditManagementBackendApplication.class, args);
+    }
+
+}
---
+++ credit-management-backend/src/test/java/ma/enset/oussama/bouhouch/creditmanagementbackend/CreditManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.oussama.bouhouch.creditmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
